03c7526d605ffbcd42806f162ed6dd6e5b8b92c9
Mareak <mareak@mareak.com>
2019-11-17 15:10:12 +0100

    fixin

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 TEMPLATE_PATH="packer/templates/debian.json"
+TEMPLATE1_PATH="packer/templates/debian1.json"
 TEMPLATE2_PATH="packer/templates/debian2.json"
+TEMPLATE3_PATH="packer/templates/debian3.json"
+TEMPLATE4_PATH="packer/templates/debian4.json"
+LOG_PATH="packer/logs/packer_output_S0.txt"
 LOG_PATH="packer/logs/packer_output_S1.txt"
 LOG2_PATH="packer/logs/packer_output_S2.txt"
+LOG3_PATH="packer/logs/packer_output_S3.txt"
+LOG4_PATH="packer/logs/packer_output_S4.txt"
 
 run: export build
 
@@ -10,3 +16,5 @@ export:
 build:
 	packer build $(TEMPLATE_PATH) | tee $(LOG_PATH)
 	packer build $(TEMPLATE2_PATH) | tee $(LOG2_PATH)
+	packer build $(TEMPLATE3_PATH) | tee $(LOG3_PATH)
+	packer build $(TEMPLATE4_PATH) | tee $(LOG4_PATH)
--- terraform/provisioning.tf
+++ terraform/provisioning.tf
@@ -30,14 +30,28 @@ resource "azurerm_network_security_rule" "web" {
   destination_address_prefix  = "*"
 }
 
-
 data "azurerm_image" "image" {
+  name                = "debianS0"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+}
+
+data "azurerm_image" "image1" {
   name                = "debianS1"
   resource_group_name = "${azurerm_resource_group.main.name}"
 }
 
 data "azurerm_image" "image2" {
   name                = "debianS2"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+}
+
+data "azurerm_image" "image3" {
+  name                = "debianS3"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+}
+
+data "azurerm_image" "image4" {
+  name                = "debianS4"
   resource_group_name = "${azurerm_resource_group.main.name}"
 }
 
@@ -62,31 +76,45 @@ resource "azurerm_subnet" "internal" {
   address_prefix       = "10.0.2.0/24"
 }
 
-resource "azurerm_network_interface" "nicS1" {
-  name                = "${var.prefix}-nicS1"
+resource "azurerm_network_interface" "nicS0" {
+  name                = "${var.prefix}-nicS0"
   location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
   dns_servers = ["10.0.2.33"]
 
   ip_configuration {
-    name                          = "${var.prefix}-nicS1-ipconf"
+    name                          = "${var.prefix}-nicS0-ipconf"
     subnet_id                     = "${azurerm_subnet.internal.id}"
     private_ip_address_allocation = "Static"
     private_ip_address = "10.0.2.33"
   }
 }
 
-resource "azurerm_network_interface" "nicS12" {
-  name                = "${var.prefix}-nicS12"
+resource "azurerm_network_interface" "nicS0" {
+  name                = "${var.prefix}-nicS0"
   location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
   network_security_group_id = "${azurerm_network_security_group.main.id}"
 
   ip_configuration {
-    name                          = "${var.prefix}-nicS12-ipconf"
+    name                          = "${var.prefix}-nicS0-ipconf"
     subnet_id                     = "${azurerm_subnet.internal.id}"
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id = "${azurerm_public_ip.pip1.id}"
+  }
+}
+
+resource "azurerm_network_interface" "nicS01" {
+  name                = "${var.prefix}-nicS01"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  dns_servers = ["10.0.2.33"]
+
+  ip_configuration {
+    name                          = "${var.prefix}-nicS01-ipconf"
+    subnet_id                     = "${azurerm_subnet.internal.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address = "10.0.2.34"
   }
 }
 
@@ -100,7 +128,66 @@ resource "azurerm_network_interface" "nicS2" {
     name                          = "${var.prefix}-nicS2-ipconf"
     subnet_id                     = "${azurerm_subnet.internal.id}"
     private_ip_address_allocation = "Static"
-    private_ip_address = "10.0.2.34"
+    private_ip_address = "10.0.2.35"
+  }
+}
+
+resource "azurerm_network_interface" "nicS3" {
+  name                = "${var.prefix}-nicS3"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  dns_servers = ["10.0.2.33"]
+
+  ip_configuration {
+    name                          = "${var.prefix}-nicS3-ipconf"
+    subnet_id                     = "${azurerm_subnet.internal.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address = "10.0.2.36"
+  }
+}
+
+resource "azurerm_network_interface" "nicS4" {
+  name                = "${var.prefix}-nicS4"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  dns_servers = ["10.0.2.33"]
+    
+  ip_configuration {
+    name                          = "${var.prefix}-nicS4-ipconf"
+    subnet_id                     = "${azurerm_subnet.internal.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address = "10.0.2.37"
+  }
+}
+
+resource "azurerm_virtual_machine" "S0" {
+    name                  = "S0"
+    location              = "${azurerm_resource_group.main.location}"
+    resource_group_name   = "${azurerm_resource_group.main.name}"
+    network_interface_ids = ["${azurerm_network_interface.nicS0.id}", "${azurerm_network_interface.nicS01.id}"]
+    primary_network_interface_id = "${azurerm_network_interface.nicS0.id}"
+    vm_size               = "Standard_DS1_v2"
+
+   storage_image_reference {
+    id="${data.azurerm_image.image.id}"
+  }
+
+  storage_os_disk {
+    name              = "${var.prefix}-osdisk-S0"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "S0"
+    admin_username = "mareak"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
   }
 }
 
@@ -108,12 +195,11 @@ resource "azurerm_virtual_machine" "S1" {
     name                  = "S1"
     location              = "${azurerm_resource_group.main.location}"
     resource_group_name   = "${azurerm_resource_group.main.name}"
-    network_interface_ids = ["${azurerm_network_interface.nicS12.id}", "${azurerm_network_interface.nicS1.id}"]
-    primary_network_interface_id = "${azurerm_network_interface.nicS12.id}"
+    network_interface_ids = ["${azurerm_network_interface.nicS1.id}"]
     vm_size               = "Standard_DS1_v2"
 
    storage_image_reference {
-    id="${data.azurerm_image.image.id}"
+    id="${data.azurerm_image.image1.id}"
   }
 
   storage_os_disk {
@@ -164,3 +250,64 @@ resource "azurerm_virtual_machine" "S2" {
     environment = "staging"
   }
 }
+
+resource "azurerm_virtual_machine" "S3" {
+    name                  = "S3"
+    location              = "${azurerm_resource_group.main.location}"
+    resource_group_name   = "${azurerm_resource_group.main.name}"
+    network_interface_ids = ["${azurerm_network_interface.nicS3.id}"]
+    vm_size               = "Standard_DS1_v2"
+
+   storage_image_reference {
+    id="${data.azurerm_image.image3.id}"
+  }
+
+  storage_os_disk {
+    name              = "${var.prefix}-osdisk-S3"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "S3"
+    admin_username = "mareak"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_virtual_machine" "S4" {
+    name                  = "S4"
+    location              = "${azurerm_resource_group.main.location}"
+    resource_group_name   = "${azurerm_resource_group.main.name}"
+    network_interface_ids = ["${azurerm_network_interface.nicS4.id}"]
+    vm_size               = "Standard_DS1_v2"
+
+   storage_image_reference {
+    id="${data.azurerm_image.image4.id}"
+  }
+
+  storage_os_disk {
+    name              = "${var.prefix}-osdisk-S4"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "S4"
+    admin_username = "mareak"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+
