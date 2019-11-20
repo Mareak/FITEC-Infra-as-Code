c6eae524668d3177a78b99ea671f8790fa9705fc
Mareak <mareak@mareak.com>
2019-11-20 10:27:38 +0100

    fixin

--- terraform/provisioning.tf
+++ terraform/provisioning.tf
@@ -10,6 +10,20 @@ resource "azurerm_resource_group" "main" {
         location = "eastus"
 }
 
+resource "azurerm_storage_account" "main" {
+  name                     = "mareakp1storageacc"
+  resource_group_name      = "${azurerm_resource_group.main.name}"
+  location                 = "${azurerm_resource_group.main.location}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_share" "main" {
+  name                 = "mareak-p1-share"
+  storage_account_name = "${azurerm_storage_account.main.name}"
+  quota                = 5
+}
+
 resource "azurerm_network_security_group" "main" {
   name     = "${var.prefix}-webservers"
   location = "${azurerm_resource_group.main.location}"
