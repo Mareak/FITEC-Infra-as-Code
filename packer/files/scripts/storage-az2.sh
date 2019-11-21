357330eb1c19615809fbf8628daad2e41f732095
Mareak <mareak@mareak.com>
2019-11-21 12:15:36 +0100

    fixin

--- packer/files/scripts/storage-az2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-storage="mareakp1storageacc.file.core.windows.net/mareak-p1-share-sql"
-username="mareakp1storageacc"
-password="XGkSBVz5Yf2YDrUexMVi5lWR5cOxmeI+GGsm0PwOsde2cxJ+5vinpRk38Cs4c4QBHA7oFx2aeBacbVO346VFhQ=="
-
-echo "//${storage} /var/lib/mysql _netdev,nofail,vers=3.0,username=${username},password=${password},dir_mode=0755,file_mode=0755,serverino" >> /etc/fstab
